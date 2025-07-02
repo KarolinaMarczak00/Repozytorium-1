0bbade9e8313eae606a9dfd5b2dffd1f1181b6b0
Karolina Marczak <marczakkarolina07@gmail.com>
2025-07-02 06:25:05 +0200

    dodano klasę Powitanie

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,4 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world! ");
     }
+class Powitanie {
+    public static void main(String[] args) {
+        System.out.println("Cześć! ");
+    }
 }
