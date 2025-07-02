05ebd7ea48b04c4c9e9739390ce25bd3f757d153
Karolina Marczak <marczakkarolina07@gmail.com>
2025-07-02 06:27:35 +0200

    Usunięto publiczną metodę

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         System.out.println("Hello world! ");
     }
 class Powitanie {
